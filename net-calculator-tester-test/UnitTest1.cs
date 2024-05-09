f711ad0acf118d55727c86a6afd127db0b12d87c
Valerio Bartoletti <valerio.bartoletti97@gmail.com>
2024-05-09 14:19:08 +0200

    Add project files.

---
+++ net-calculator-tester-test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace net_calculator_tester_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

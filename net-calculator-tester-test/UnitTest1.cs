da9fc99986d41e0b50652b6076f7da440fe39f9b
Valerio Bartoletti <valerio.bartoletti97@gmail.com>
2024-05-09 15:29:17 +0200

    exercise finished

--- net-calculator-tester-test/UnitTest1.cs
+++ net-calculator-tester-test/UnitTest1.cs
@@ -17,5 +17,44 @@ namespace net_calculator_tester_test
             var result = num1 + num2;
             Assert.AreEqual(Calculator.Add(num1, num2),result);
         }
+
+        [Test]
+        [TestCase(1.3f, 1.7f)]
+        [TestCase(-1.3f, -1.7f)]
+        [TestCase(1.3f, -1.7f)]
+        public void TestSubstract(float num1, float num2)
+        {
+            var result = num1 - num2;
+            Assert.AreEqual(Calculator.Substract(num1, num2), result);
+        }
+
+        [Test]
+        [TestCase(12, 3)]
+        [TestCase(-1.3f, 0)]
+        [TestCase(1, -1)]
+        public void TestMultiply(float num1, float num2)
+        {
+            var result = num1 * num2;
+            Assert.AreEqual(Calculator.Multiply(num1, num2), result);
+        }
+
+
+        [Test]
+        [TestCase(12, 3)]
+        [TestCase(-1.3f, 0)]
+        [TestCase(1, -1)]
+        public void TestDivide(float num1, float num2)
+        {
+            if(num2 == 0)
+            {
+            Assert.Throws<System.ArgumentException>(() => { Calculator.Divide(num1, num2); },
+           "Stai tentando di dividere per zero per zero");
+            }
+            else
+            {
+                var result = num1 * num2;
+                Assert.AreEqual(Calculator.Multiply(num1, num2), result);
+            }
+        }
     }
 }
